c5d3d1a248a91853a1a6d62605f1376b0d7c0a6d
dgtoa <dgtoa0@gmail.com>
2017-05-29 23:50:16 +0900

    05.29 11:45 fork 후 캘린더 추가

--- app/src/main/java/seungchan/com/tobaccoach_5_2/fragment/CalendarFragment.java
+++ app/src/main/java/seungchan/com/tobaccoach_5_2/fragment/CalendarFragment.java
@@ -1,6 +1,7 @@
 package seungchan.com.tobaccoach_5_2.fragment;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,19 +9,63 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.stacktips.view.CalendarListener;
+import com.stacktips.view.CustomCalendarView;
+import com.stacktips.view.DayDecorator;
+import com.stacktips.view.DayView;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import seungchan.com.tobaccoach_5_2.R;
+import seungchan.com.tobaccoach_5_2.dao.TobaccoDaoService;
 
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link CalendarFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link CalendarFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
 public class CalendarFragment extends Fragment {
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static String TAG = "CalendarFragment";
 
-    private String mParam1;
-    private String mParam2;
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "ipAddress";
+    private static final String ARG_PARAM2 = "userId";
+
+    // TODO: Rename and change types of parameters
+    private String ipAddress;
+    private String userId;
+
+    private TobaccoDaoService tobaccoDaoService;
+
+    @BindView(R.id.calendar_view)
+    CustomCalendarView calendarView;
 
     private OnFragmentInteractionListener mListener;
 
-    public CalendarFragment() { }
+    public CalendarFragment() {
+        // Required empty public constructor
+    }
 
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment CalendarFragment.
+     */
+    // TODO: Rename and change types and number of parameters
     public static CalendarFragment newInstance(String param1, String param2) {
         CalendarFragment fragment = new CalendarFragment();
         Bundle args = new Bundle();
@@ -34,18 +79,80 @@ public class CalendarFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            ipAddress = getArguments().getString(ARG_PARAM1);
+            userId = getArguments().getString(ARG_PARAM2);
         }
+        tobaccoDaoService = TobaccoDaoService.getInstance(getActivity());
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View calendarFragmentView = inflater.inflate(R.layout.fragment_calendar, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View calendarFragmentView=  inflater.inflate(R.layout.fragment_calendar, container, false);
+        ButterKnife.bind(this, calendarFragmentView);
+
+        //뭔가를 출력 후 리턴
+//Initialize calendar with date
+        Calendar currentCalendar = Calendar.getInstance(Locale.getDefault());
+
+        //Show sunday as first date of week
+        calendarView.setFirstDayOfWeek(Calendar.SUNDAY);
+
+        //Show/hide overflow days of a month
+        calendarView.setShowOverflowDate(false);
+
+        //call refreshCalendar to update calendar the view
+        calendarView.refreshCalendar(currentCalendar);
+
+        //Handling custom calendar events
+        calendarView.setCalendarListener(new CalendarListener() {
+            @Override
+            public void onDateSelected(Date date) {
+                //if (!CalendarUtils.isPastDay(date)) {
+                //    SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+                //    selectedDateTv.setText("Selected date is " + df.format(date));
+                //} else {
+                //    selectedDateTv.setText("Selected date is disabled!");
+                //}
+            }
+
+            @Override
+            public void onMonthChanged(Date date) {
+                //SimpleDateFormat df = new SimpleDateFormat("MM-yyyy");
+                //Toast.makeText(CalendarDayDecoratorActivity.this, df.format(date), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        //adding calendar day decorators
+        List<DayDecorator> decorators = new ArrayList<>();
+        decorators.add(new OveredColorDecorator());
+        calendarView.setDecorators(decorators);
+        calendarView.refreshCalendar(currentCalendar);
 
         return calendarFragmentView;
     }
 
+    private class OveredColorDecorator implements DayDecorator {
+        @Override
+        public void decorate(DayView dayView) {
+            Calendar c= Calendar.getInstance();
+            c.setTime(dayView.getDate());
+
+            SimpleDateFormat formatter = new SimpleDateFormat( "yyyy.MM.dd HH:mm:ss", Locale.getDefault() );
+            String dateStr= formatter.format ( dayView.getDate() );
+            int todayAmount= tobaccoDaoService.getAmountByDay(dateStr);
+            int averageAmount= tobaccoDaoService.getAverageLogAmount();
+
+            if (todayAmount > averageAmount) { //특정 날짜가 평균을 넘었다면 색칠
+                int color = Color.parseColor("#FF1A5B");
+                dayView.setBackgroundColor(color);
+            }
+        }
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -69,7 +176,21 @@ public class CalendarFragment extends Fragment {
         mListener = null;
     }
 
+
+
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
     public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
 }
